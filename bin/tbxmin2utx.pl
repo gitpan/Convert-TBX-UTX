030a62c28b247175a5203df72537587d1a0e9d9e
BYU TRG <byutrg@cpan.org>
2014-02-04 00:22:52 +0000

    Import of BYUTRG/Convert-TBX-UTX-0.01 from CPAN.
    gitpan-cpan-distribution: Convert-TBX-UTX
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         BYUTRG/Convert-TBX-UTX-0.01.tar.gz
    gitpan-cpan-author:       BYUTRG
    gitpan-cpan-maturity:     released

---
+++ bin/tbxmin2utx.pl
@@ -0,0 +1,7 @@
+#!usr/bin/perl
+
+use strict;
+use warnings;
+use Convert::TBX::UTX qw(min2utx);
+
+min2utx(@ARGV);
---
+++ bin/utx2tbxmin.pl
@@ -0,0 +1,7 @@
+#!usr/bin/perl
+
+use strict;
+use warnings;
+use Convert::TBX::UTX qw(utx2min);
+
+utx2min(@ARGV);
